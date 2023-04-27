2463a98f09ffd4eb5ee08943ea37a5a2194569b9
VeroCaraminola <caraminolaveronica@gmail.com>
2023-04-27 16:45:30 -0300

    Estructura del proyecto

---
+++ proyectofinal_caraminolaveronica/backend/src/main/java/com/portfolio/vcc/VccApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.vcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VccApplication.class, args);
+	}
+
+}
---
+++ proyectofinal_caraminolaveronica/backend/src/test/java/com/portfolio/vcc/VccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.vcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
