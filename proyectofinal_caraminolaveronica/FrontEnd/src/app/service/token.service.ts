df8049d397741b2baa75d2197d169b527191aeec
VeroCaraminola <caraminolaveronica@gmail.com>
2023-05-15 19:08:24 -0300

    token service

--- proyectofinal_caraminolaveronica/FrontEnd/src/app/service/token.service.ts
+++ proyectofinal_caraminolaveronica/FrontEnd/src/app/service/token.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 const TOKEN_KEY = 'AuthToken';
 const USERNAME_KEY = 'AuthUsername';
 const AUTHORITIES_KEY = 'AuthAuthorities';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,29 +14,42 @@ export class TokenService {
 
   constructor() { }
 
-  public setToken(token: String): void {
+  public setToken(token: string) : void {
     window.sessionStorage.removeItem(TOKEN_KEY);
     window.sessionStorage.setItem(TOKEN_KEY, token);
 }
 
-public getToken():string {
-    return sessionStorage.getItem(TOKEN_KEY)!;
-}
 
-public setUserName(userName:String):void {
-  window.sessionStorage.removeItem(USERNAME_KEY);
-  window.sessionStorage.setItem(USERNAME_KEY,userName);
+   public getToken():string {
+    return sessionStorage.getItem(TOKEN_KEY)!;
+} 
+
+   public setUserName (userName:string):void {
+  window.sessionStorage.removeItem(USERNAME_KEY),
+  window.sessionStorage.setItem(USERNAME_KEY, userName);
 
 }
 
 public getUserName():string {
   return sessionStorage.getItem(TOKEN_KEY)!;
 }
-public setAuthorities(authorities:String[]):void {
+public setAuthorities(authorities:string[]):void {
   window.sessionStorage.removeItem(AUTHORITIES_KEY);
-  window.sessionStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities);
+  window.sessionStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities));
 
 }
 
-
+public getAuthorities(): string [] {
+  this.roles = [];
+  if(sessionStorage.getItem(AUTHORITIES_KEY)){
+    JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)!).forEach((authority:any) => {
+    this.roles.push(authority.authority);
+  });
+  
+}
+return this.roles;
+}
+  public logOut(): void {
+  window.sessionStorage.clear();
+}
 }
